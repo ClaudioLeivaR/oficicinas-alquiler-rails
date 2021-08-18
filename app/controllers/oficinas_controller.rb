ab4587bc9dcc3d93dee4c08364820bd6853efe8f
Claudio <claudio.leiva.r@gmail.com>
2021-08-18 09:42:09 -0400

    ejercicio dia 3 oficinas

---
+++ app/controllers/oficinas_controller.rb
@@ -0,0 +1,25 @@
+class OficinasController < ApplicationController
+  def listar
+
+    @lista_oficinas = Oficina.all
+
+  end
+
+  def crear
+    @oficina_nueva = Oficina.new
+  end
+
+  def guardar
+    valores = params.require(:oficina).permit(:codigo, :precio, :esta_alquilada)
+    @oficina_nueva = Oficina.new(valores)
+
+    if @oficina_nueva.save
+      redirect_to action:"listar"
+    else
+      render :crear
+    end
+
+  end
+
+
+end
---
+++ app/models/oficina.rb
@@ -0,0 +1,7 @@
+class Oficina < ApplicationRecord
+
+    validates :codigo, uniqueness: true
+    validates :codigo, presence: true
+    validates :precio, presence: true
+    
+end
